720d315e47031bb9ef183d3792974300b28427e2
Faisal Amiruddin <faisalamrdn@gmail.com>
2025-11-07 11:09:21 +0700

    Add componen TradingView

---
+++ src/components/StockChart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="tradingview_chart" style="height: 500px;"></div>
+</template>
+
+<script>
+export default {
+  name: "StockChart",
+  mounted() {
+    new TradingView.widget({
+      container_id: "tradingview_chart",
+      width: "100%",
+      height: 500,
+      symbol: "IDX:CDIA", // kalau saham Indonesia → pakai prefix IDX:
+      interval: "D",
+      timezone: "Asia/Jakarta",
+      theme: "dark",
+      style: "1",
+      locale: "id",
+      toolbar_bg: "#1f2937",
+      enable_publishing: false,
+      hide_top_toolbar: false,
+      hide_legend: false,
+      withdateranges: true,
+      allow_symbol_change: true
+    });
+  }
+};
+</script>
+
+<style scoped>
+#tradingview_chart {
+  border-radius: 10px;
+}
+</style>
